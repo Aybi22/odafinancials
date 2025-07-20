aa6a2b3ce03f4d15ded0cbae23e110de1601a7ef
Aybi22 <your_email_address@example.com>
2025-07-20 14:19:16 +0100

    "add images"

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ fetch(" ./header.html")
     let headnav = document.getElementById("headnav");
     headnav.innerHTML = html;
   });
+
+fetch(" ./footer.html")
+  .then((response) => response.text())
+  .then((html) => {
+    let footer = document.getElementById("footer");
+    footer.innerHTML = html;
+  });
