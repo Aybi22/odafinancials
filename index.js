d18538a63f17aae6cdce1b90eb6374c71461ceed
Aybi22 <your_email_address@example.com>
2025-07-26 20:38:50 +0100

    "create sliding image carousel  in help section"

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ fetch(" ./footer.html")
     let footer = document.getElementById("footer");
     footer.innerHTML = html;
   });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let slider = document.getElementById("slider");
+  const images = slider.querySelectorAll("img");
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    if (index < 0) {
+      currentIndex = images.length - 1;
+    } else if (index >= images.length) {
+      currentIndex = 0;
+    } else {
+      currentIndex = index;
+    }
+    slider.style.transform = `translateX(-${currentIndex * 1200}px)`;
+  }
+  function nextSlide() {
+    showSlide(currentIndex + 1);
+  }
+
+  function previousSlide() {
+    showSlide(currentIndex - 1);
+  }
+
+  setInterval(nextSlide, 5000);
+});
