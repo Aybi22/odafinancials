e8f047b17404e03679428ff63e8009ef2c7185b0
Aybi22 <your_email_address@example.com>
2025-10-04 05:24:17 +0100

    "update service cards section"

--- rates.js
+++ rates.js
@@ -5,38 +5,59 @@ fetch("services.json")
   .then((services) => {
     let thisService = services.find((card) => card.id === serviceId);
     let detail = document.querySelector(".details");
+    detail.classList.add("card-container");
     if (thisService) {
       detail.innerHTML = `
     
    
-      <article>
-       <h3>${thisService.name}</h3>
-    <div class="image">
+   
+   
+      <div class="service-card">
+     <h3 class="service-name">${thisService.name}</h3> 
+      
+    <p class="bold-title">service description</p>
+  <p>${thisService.description}</p>
+    <div class="service-block">   
+  
+  <div class="image">
     <img src="${thisService.image}">
     
-    </div>
 
+</div>
+    
 
-<pre>
-<strong>Tier 1: Basic – £49 per service</strong>
-• Entry-level package
-• Covers essentials only
-• Best for individuals & sole traders
+<div class="pricing-section">
 
-<strong>Tier 2: Standard ⭐ (Most Popular) – £99 per service</strong>
-• Mid-tier package
-• Balanced support + added value
-• Ideal for startups & small businesses
+<ul class="pricing">
+<h4>Tier 1: Basic – £49 per service</h4>
+<li> Entry-level package</li>
+<li> Covers essentials only</li>
+<li> Best for individuals & sole traders</li>
 
-<strong>Tier 3: Premium – £199 per service</strong>
-• Comprehensive package
-• High-touch, tailored support
-• Designed for growing businesses
-</pre>
+<h4>Tier 2: Standard ⭐ (Most Popular) – £99 per service</h4>
+ <li>Mid-tier package</li>
+<li> Balanced support + added value</li>
+ <li>Ideal for startups & small businesses</li>
 
+<h4>Tier 3: Premium – £199 per service</h4>
+ <li>Comprehensive package</li>
+ <li>High-touch, tailored support</li>
+ <li>Designed for growing businesses</li>
 
+</ul>
 <a class="btn">book now</a>
-</article>
+
+
+
+</div>
+
+</div>
+</div>
+</div>
+
+
     `;
+
+      detail.appendChild(cardContainer);
     }
   });
